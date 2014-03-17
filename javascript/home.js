f061798870e9875acea6b80877debe2d9cb2aaed
Scott Morton <scottmorton@Scotts-MacBook-Pro-3.local>
2014-03-16 22:56:24 -0500

    update1

--- javascript/home.js
+++ javascript/home.js
@@ -1,71 +1,59 @@
 var video = new Array();
 
-	for (var i = 0; i < 14; i++) {
+	for (var i = 0; i < 100; i++) {
     	video[i] = new Array("","");
   	}
 	
 	video[0][0]="B4PEXLODw9c";
-	video[0][1]="SpaceX grasshopper test platform hovers at 12 Stories";
+	video[0][1]="SpaceX Grasshopper Test Platform Hovers at 12 Stories";
 	
 	video[1][0]="pjAIwtdAyQA"
-	video[1][1]="NASA lunar lander test flight";
+	video[1][1]="NASA Lunar Lander Test Flight";
 	
 	video[2][0]="i7yPVaNdGBw";
-	video[2][1]="Dream Chaser space shuttle concept video";
+	video[2][1]="Dream Chaser Space Shuttle Concept Video";
 	
 	video[3][0]="0sRYJ6X6BEY";
-	video[3][1]="International space station footage";
+	video[3][1]="International Space Station Footage";
 	
 	video[4][0]="SuR6sDMAdXs";							
-	video[4][1]="Blue origin escape vehicle test";
+	video[4][1]="Blue Origin Escape Vehicle Test";
 
 	video[5][0]="voYsnECLduQ";
-	video[5][1]="Mars curiosity landing";
+	video[5][1]="Mars Curiosity Landing";
 	
 	video[6][0]="WlGemHL5vLY";
-	video[6][1]="Audacious visions";
+	video[6][1]="Neil deGrasse Tyson: 'Audacious Visions'";
 	
 	video[7][0]="hBnO8lJd2JU";
-	video[7][1]="Nasa's future missions";
+	video[7][1]="Nasa's Future Missions";
 
-	video[8][0]="7i2QDpGRQKc"
-	video[8][1]="Niel Tyson on the NASA budget"
+	video[8][0]="dohU61DuEaI"
+	video[8][1]="Niel deGrasse Tyson: Science and Politics"
 
 	video[9][0]="TQ6tZtGrShg";
-	video[9][1]="Elon Musk gives a tour of SpaceX";
+	video[9][1]="Elon Musk Gives a Tour of SpaceX";
 	
 	video[10][0]="TysxQ9lPeKY";
 	video[10][1]="Michio Kaku on the Space Program";
 	
 	video[11][0]="SBiY0Fn1ze4";
-	video[11][1]="Gravity probe B experiment results";
+	video[11][1]="Gravity Probe B Experiment Results";
 	
 	video[12][0]="YvypMHkUpKo";
-	video[12][1]="Skylab, the first space station";
+	video[12][1]="Skylab, the First Space Station";
 	
-	video[13][0]="Ajvkbe1zjjs";							
-	video[13][1]="The Space Race";
-
+	video[13][0]="HXdjxPY2j_0";
+	video[13][1]="SpaceX Grasshopper Divert Maneuver";
+	
+	
+	
 
 $(document).ready(function() {
 	
 	populate_thumbnails(video, "#thumbnails");
 	$('#iframe-player').attr('src', 'http://www.youtube.com/embed/'+video[0][0]);
-	
-	$(".selector-tab").live('click', function() {
-	
-		if	($(this).attr("id") == "recent")
-		{
-			populate_thumbnails(video, "#thumbnails");
-		}
-		
-		if	($(this).attr("id") == "video")
-		{
-			populate_thumbnails(video, "#thumbnails");
-		}
-		
 
-	});
 	
 	$(".video").live('click', function() {
 
@@ -82,13 +70,24 @@ function populate_thumbnails(array, divId)
 	
 	var htmlString = '';
 	
-	$.each(array, function(index, value) { 
-	 
-		htmlString= htmlString+
-		
-		'<div class="video" id='+value[0]+'><div class="thumbnail"> <img src="http://img.youtube.com/vi/'+value[0]+'/3.jpg"></div>  <div class="description"> <p class="desc">'+ value[1]+'</p> </div></div> ';
 	
+	for (var i = array.length-1; i > -1; i--) {
+		if(array[i][0]!="")
+		{
+		htmlString= htmlString+
+		'<div class="video" id='+array[i][0]+'><div class="thumbnail"> <img src="http://img.youtube.com/vi/'+array[i][0]+'/3.jpg"></div>  <div class="description"> <p class="desc">'+ array[i][1]+'</p> </div></div> ';
+		}
+  	}
+	
+	/*
+	$.each(array, function(index, value) { 
+		if(value[0]!="")
+		{
+		htmlString= htmlString+
+		'<div class="video" id='+value[0]+'><div class="thumbnail"> <img src="http://img.youtube.com/vi/'+value[0]+'/3.jpg"></div>  <div class="description"> <p class="desc">'+ value[1]+'</p> </div></div> ';
+		}
 	});
+	*/
 	
 	$(divId).fadeOut(300);
 
